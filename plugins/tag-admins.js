21df20cbe0cd83b6e86a74d55482fec62bc90352
JOENZAI007 <ahmadmehar1127@gmail.com>
2026-06-02 20:15:07 +0500

    Update tag-admins.js

--- plugins/tag-admins.js
+++ plugins/tag-admins.js
@@ -41,7 +41,7 @@ async (conn, mek, m, { from, participants, reply, isGroup, senderNumber, groupAd
             teks += `${randomEmoji} @${admin.split('@')[0]}\n`;
         }
 
-        teks += "└──✪ ANAYAT┃ AI ✪──";
+        teks += "└──✪ JOEN-MEHAR007 ✪──";
 
         conn.sendMessage(from, { text: teks, mentions: admins }, { quoted: mek });
 
